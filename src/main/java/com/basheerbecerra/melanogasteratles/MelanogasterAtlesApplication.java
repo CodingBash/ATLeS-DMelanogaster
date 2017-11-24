4141895d5cb92bd7db3f980ad82f8358a33bd90b
Basheer@raspi <codingbash@gmail.com>
2017-11-24 17:14:45 +0000

    Initial commit

---
+++ src/main/java/com/basheerbecerra/melanogasteratles/MelanogasterAtlesApplication.java
@@ -0,0 +1,12 @@
+package src.main.java.com.basheerbecerra.melanogasteratles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelanogasterAtlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelanogasterAtlesApplication.class, args);
+	}
+}
