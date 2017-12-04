82cd67edd9f4dc58449d9d252c5a71e736b73f35
CodingBash <bbecerr@outlook.com>
2017-12-04 14:27:28 -0600

    Added commands to remove previous files, therefore a second run works

--- src/main/java/com/basheerbecerra/melanogasteratles/controllers/MainController.java
+++ src/main/java/com/basheerbecerra/melanogasteratles/controllers/MainController.java
@@ -39,35 +39,47 @@ public class MainController {
 			@RequestParam(value = "seconds", defaultValue = "5") Integer seconds)
 			throws IOException, InterruptedException {
 		Runtime run = Runtime.getRuntime();
-		// Capture Video
+
+		/*
+		 * Make directory to contain captured video from pi
+		 */
 		Process makeCaptureDir = run.exec("mkdir ./src/main/resources/capture");
 		System.out.println(makeCaptureDir.waitFor());
 
+		/*
+		 * Take the video (only works on pi environment)
+		 */
 		Process takeVideo = run.exec("raspivid -o ./src/main/resources/capture/capture.h264 -t " + seconds * 1000);
 		System.out.println(takeVideo.waitFor());
 
+		/*
+		 * Convert the video from .h264 to .mp4 (only works on environment w/ MP4Box)
+		 */
 		Process convertVideo = run
 				.exec("MP4Box -add ./src/main/resources/capture/capture.h264 ./src/main/resources/capture/capture.mp4");
 		System.out.println(convertVideo.waitFor());
 
+		/*
+		 * Remove any previous capture from static folder
+		 */
 		Process removePreviousVideo = run.exec("rm -f ./src/main/resources/static/capture.mp4");
-		removePreviousVideo.waitFor();
+		System.out.println(removePreviousVideo.waitFor());
 
+		/*
+		 * Move the capture result to the static folder
+		 */
 		Process moveVideoToStatic = run.exec("mv ./src/main/resources/capture/capture.mp4 ./src/main/resources/static");
 		System.out.println(moveVideoToStatic.waitFor());
 
-		Thread.sleep(6000);
-		System.out.println("Here");
-
-		redirectAttributes.addFlashAttribute("test", "test");
-
+		/*
+		 * Redirect to the result page
+		 */
 		return "redirect:/results";
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/results")
 	public ModelAndView results(@ModelAttribute("test") String test) {
 		ModelAndView mav = new ModelAndView("results");
-		mav.addObject("test", test);
 		return mav;
 	}
 
@@ -75,48 +87,72 @@ public class MainController {
 	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
 			throws InterruptedException, IOException {
 		Runtime run = Runtime.getRuntime();
-
+		
+		/*
+		 * Delete everything from static directory
+		 */
+		service.deleteAll();
+		/*
+		 * Store the uploaded file
+		 */
 		service.store(file);
 
+		/*
+		 * Run the OpenCV analysis on the uploaded file
+		 */
 		Process runAnalysis = run.exec("python analyze.py");
-		System.out.println(runAnalysis.isAlive());
-		System.out.println(runAnalysis.waitFor());
-		
-		Process convert = run.exec("ffmpeg -i src/main/resources/static/capture.avi -c:v libx264 -crf 19 -preset slow -c:a libfdk_aac -b:a 192k -ac 2 src/main/resources/static/capture.mp4");
-		InputStream stderr = convert.getErrorStream();
-        InputStreamReader isr = new InputStreamReader(stderr);
-        BufferedReader br = new BufferedReader(isr);
-        String line = null;
-        System.out.println("<ERROR>");
-        while ( (line = br.readLine()) != null)
-            System.out.println(line);
-        System.out.println("</ERROR>");
-		System.out.println(convert.waitFor());
-		while (true) {
-			File f = new File("src/main/resources/static/capture.mp4");
-			if (f.exists() && !f.isDirectory()) {
-				break;
+		int analysisExitCode = runAnalysis.waitFor();
+
+		/*
+		 * If analysis was successful
+		 */
+		if (analysisExitCode == 0) {
+			/*
+			 * Convert the analysis result video from .avi to .mp4 (only works with environment with ffmpeg installed)
+			 */
+			Process convert = run.exec(
+					"ffmpeg -i src/main/resources/static/capture.avi -c:v libx264 -crf 19 -preset slow -c:a libfdk_aac -b:a 192k -ac 2 src/main/resources/static/capture.mp4");
+			
+			/*
+			 * Stream feed to prevent deadlock
+			 */
+			InputStream stderr = convert.getErrorStream();
+			InputStreamReader isr = new InputStreamReader(stderr);
+			BufferedReader br = new BufferedReader(isr);
+			String line = null;
+			while ((line = br.readLine()) != null)
+				System.out.println(line);
+			int convertExitCode = convert.waitFor();
+			
+			/*
+			 * If conversion was successful
+			 */
+			if (convertExitCode == 0) {
+				/*
+				 * Redirect to result page
+				 */
+				return "redirect:/results";
 			}
 		}
+		
+		/*
+		 * If an errors, redirect back to home page 
+		 * TODO: Add flash attribute with error
+		 */
+		return "redirect:/";
 
-		System.out.println("Complete");
-		return "redirect:/results";
 	}
 
 	@RequestMapping(value = "/video", method = RequestMethod.GET)
 	public void getFile(HttpServletResponse response) throws IOException {
 		try {
 			response.setContentType("video/mp4");
-			// get your file as InputStream
 			FileSystemResource resource = new FileSystemResource("src/main/resources/static/capture.mp4");
 			InputStream is = resource.getInputStream();
-			// copy it to response's OutputStream
 			IOUtils.copy(is, response.getOutputStream());
 			response.flushBuffer();
 		} catch (IOException ex) {
-			response.flushBuffer();
-			// throw new RuntimeException("IOError writing file to output
-			// stream");
+			throw new RuntimeException("IOError writing file to output stream");
 		}
 
 	}
